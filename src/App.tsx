5473682a0c2b8d1f51b96e8a9fe168f36b2b773e
sooliter <jacksonptanner@gmail.com>
2025-05-12 23:31:06 -0700

    updated chakra, semi-parity w/ old version

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,71 @@
-//import { animate } from 'animejs'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import './App.css'
+// import { animate, waapi } from 'animejs'
+import { Accordion, HStack, RadioCard, Span, Stack } from '@chakra-ui/react'
+// import { useEffect } from "react"
 
-function App() {
-  const [count, setCount] = useState(0) 
+export default function App() {
+  // list of categories and their counts
+  const cats = [
+    {title: "Action", count: 8},
+    {title: "Drama", count: 8},
+    {title: "Fun", count: 8},
+    {title: "Love", count: 8},
+    {title: "Comedy", count: 7},
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <GenerateCategories cats={cats} />
   )
 }
 
-export default App
+// construct accordions for each category
+// input = list of categories and their counts
+const GenerateCategories = ({ cats }) => {
+  return (
+    <Accordion.Root multiple variant="subtle">
+      {cats.map((cat, index) => (
+        <Accordion.Item key={index} value={cat.title}>
+          <Accordion.ItemTrigger>
+            <Span flex="1">{cat.title}</Span>
+            <Accordion.ItemIndicator />
+          </Accordion.ItemTrigger>
+          <Accordion.ItemContent>
+            <Accordion.ItemBody>
+              <GenerateVoting title={cat.title[0]} count={cat.count} />
+            </Accordion.ItemBody>
+          </Accordion.ItemContent>
+        </Accordion.Item>
+      ))}
+    </Accordion.Root>
+  )
+}
+
+// construct video radio cards for each video in the given category
+// input = title of category, number of videos in that category
+const GenerateVoting = ({ title, count }) => {
+  return (
+    <Stack>
+      {Array.from({ length: count }, (_, row) => (
+        <RadioCard.Root
+          orientation="vertical"
+          colorPalette="blue"
+          key={`${title}-${row+1}`}
+          align="center"
+          justify="center"
+          defaultValue="next"
+        >
+          <HStack>
+            <RadioCard.Label minWidth="60px" fontSize="2em" color="blue">{title}{row+1}</RadioCard.Label>
+            {Array.from({ length: 5 }, (_, col) => (
+              <RadioCard.Item key={`${title}-${row+1}-${col+1}`} value={col+1}>
+                <RadioCard.ItemHiddenInput />
+                <RadioCard.ItemControl>
+                  <RadioCard.ItemText>{col+1}</RadioCard.ItemText>
+                </RadioCard.ItemControl>
+              </RadioCard.Item>
+            ))}
+          </HStack>
+        </RadioCard.Root>
+      ))}
+    </Stack>
+  )
+}
